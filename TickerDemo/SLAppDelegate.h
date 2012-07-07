fc07ac3dad52c299f25f8ce28955eef1d56a7f3d
Sarah Lensing <sarahlensing@gmail.com>
2012-07-07 14:53:48 -0400

    Revert "Rm old TickerDemo files"
    This reverts commit b50f8d56e56aa67e9cd1d3f0b3ff1a74f7096b9a.

---
+++ TickerDemo/SLAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  SLAppDelegate.h
+//  TickerDemo
+//
+//  Created by Sarah Lensing on 7/6/12.
+//  Copyright (c) 2012 NYU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class SLViewController;
+
+@interface SLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) SLViewController *viewController;
+
+@end
---
+++ TickerDemo/SLTickerView.m
@@ -0,0 +1,161 @@
+//
+//  SLTickerView.m
+//  TickerDemo
+//
+//  Created by Sarah Lensing on 7/6/12.
+//  Copyright (c) 2012 NYU. All rights reserved.
+//
+
+#import "SLTickerView.h"
+
+@implementation SLTickerView
+
+@synthesize delegate = _delegate;
+@synthesize flipSpeed = _flipSpeed;
+@synthesize anchorType = _anchorType;
+
+- (void)addPerspective {
+    CATransform3D myTransform = CATransform3DIdentity;
+    float zzz= 1000;
+    myTransform.m34 = -1/zzz;
+    self.layer.transform = myTransform;
+}
+
+- (void)configureAnchor {
+    self.anchorType = TickerViewAnchorBottom;
+}
+
+- (void)configureFlipSpeed {
+    self.flipSpeed = TickerViewFlipSpeedNormal;
+}
+
+- (void)configureMinMax {
+    _minX = 0;
+    _maxX = M_PI;
+}
+
+- (void)addGesture {
+    _pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(doPan:)];
+    [_pan addObserver:self forKeyPath:@"state" options:NSKeyValueObservingOptionNew context:nil];
+    [self addGestureRecognizer:_pan];
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.userInteractionEnabled = YES;
+        
+        [self addPerspective];
+        [self configureAnchor];
+        [self configureFlipSpeed];
+        [self configureMinMax];
+        [self addGesture];    
+        
+    }
+    return self;
+}
+
+- (CGFloat)valueForGesture:(UIPanGestureRecognizer *)gesture {
+    
+    CGRect myNormalized = [self convertRect:self.bounds toView:self.superview];
+    float myHeight = self.bounds.size.height;    
+    float myTop = myNormalized.origin.y;
+    float totalAvailableProgress = (myTop + 2*myHeight);
+    
+    float translation = [gesture translationInView:self.superview].y;
+    float absTranslation = fabsf(translation);
+    float myTranslation = [gesture translationInView:self].y; 
+    
+    if ((translation < 0 && fabsf(myTranslation) != INFINITY && _normalPanStart && myTranslation < 0) || 
+        (_normalPanStart && _anchorType == TickerViewAnchorTop && translation < 0) //prevent over translation if top anchor and above fold
+        ) {
+        absTranslation = 0;
+    } else if ((!_normalPanStart && translation > 0)) {
+        absTranslation = totalAvailableProgress;
+    } else {
+        absTranslation *= _flipSpeedThrottle;        
+    }
+    
+    float progress = (absTranslation / totalAvailableProgress ) * M_PI;
+    
+    if ((!_normalPanStart && translation < 0)) {
+        progress = M_PI - fabsf(progress);
+    } 
+    
+    if (_anchorType == TickerViewAnchorBottom) {
+        progress= -1*progress;  //so that it flips towards us and not away
+    } else {
+        progress = M_PI - progress;
+    }
+    //    NSLog(@"test:%f myTranslation:%f translation:%f   progress:%f",myTranslation, translation, absTranslation, progress);
+    return progress;
+}
+
+- (void)doPan:(UIPanGestureRecognizer *)gesture {
+    if (gesture.state == UIGestureRecognizerStateBegan) {
+        CGRect myNormalized = [self convertRect:self.bounds toView:self.superview];
+        _normalPanStart = (self.layer.position.y != myNormalized.origin.y);         
+    }
+    else if (gesture.state != UIGestureRecognizerStateChanged) {
+        return;
+    }
+    CGFloat y = [self valueForGesture:gesture];
+    [self updateRotationTransform:y];
+}
+
+- (void)setFlipSpeed:(TickerViewFlipSpeed)flipSpeed {
+    switch (flipSpeed) {
+        case TickerViewFlipSpeedFast:
+            _flipSpeedThrottle = 1.25;
+            break;
+        case TickerViewFlipSpeedNormal:
+            _flipSpeedThrottle = 1;
+            break;
+        case TickerViewFlipSpeedSlow:
+            _flipSpeedThrottle = .75;
+            break;
+        default:
+            break;
+    }
+    _flipSpeed = flipSpeed;
+}
+
+- (void)setAnchorType:(TickerViewAnchorType)anchorType {
+    switch (anchorType) {
+        case TickerViewAnchorTop:
+            self.layer.anchorPoint = CGPointMake(0.5, 0);
+            break;
+        case TickerViewAnchorBottom:
+            self.layer.anchorPoint = CGPointMake(0.5, 1);
+            break;
+        default:
+            break;
+    }
+    _anchorType = anchorType;
+}
+
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    int newChange = [[change objectForKey:@"new"] intValue];
+    if ((object == _pan) && (newChange == 3)) {
+        CGFloat y = fabsf([self valueForGesture:_pan]);
+        CGFloat newY;
+        if (y < M_PI_2) {
+            newY = 0;
+        } else {
+            newY = M_PI;
+        }
+        [UIView animateWithDuration:0.5 animations:^{
+            [self updateRotationTransform:newY];
+        }];
+    }
+}
+
+- (void)updateRotationTransform:(CGFloat)y {
+    [self.layer setValue:[NSNumber numberWithFloat:y] forKeyPath:@"transform.rotation.x"]; 
+    if (self.delegate) {
+        [self.delegate tickerView:self didUpdateRotationTransform:y];
+    }
+}
+
+@end
