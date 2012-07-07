75d2c829b7574b364e75ab63ac2eb1f5e60f5d46
Sarah Lensing <sarahlensing@gmail.com>
2012-07-07 14:26:19 -0400

    Add Ticker static lib target

---
+++ Ticker/Ticker.m
@@ -0,0 +1,13 @@
+//
+//  Ticker.m
+//  Ticker
+//
+//  Created by Sarah Lensing on 7/7/12.
+//  Copyright (c) 2012 NYU. All rights reserved.
+//
+
+#import "Ticker.h"
+
+@implementation Ticker
+
+@end
--- TickerDemo/SLViewController.m
+++ TickerDemo/SLViewController.m
@@ -26,13 +26,16 @@
     _ticker2 = [[SLTickerView alloc] initWithFrame:CGRectMake(50, 50, 100, 150)];
     _ticker2.layer.position = CGPointMake(80, 480/2+2);
     _ticker2.backgroundColor = [UIColor redColor];
+    _ticker2.layer.backgroundColor = [UIColor redColor].CGColor;    
     [self.view addSubview:_ticker2];
     
     UILabel *lbl = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 100, 150)];
     lbl.backgroundColor = [UIColor clearColor];
     lbl.textAlignment = UITextAlignmentCenter;
     lbl.text = @"Success.";
+    lbl.layer.opacity = 0;    
     [_ticker2 addSubview:lbl];
+    lbl.layer.backgroundColor = [UIColor greenColor].CGColor;
     
     _ticker = [[SLTickerView alloc] initWithFrame:CGRectMake(50, 50, 100, 150)];
     _ticker.layer.position = CGPointMake(320-80, 480/2);
@@ -40,11 +43,12 @@
     _ticker.delegate = self;
     [self.view addSubview:_ticker];
     
-    lbl = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 100, 150)];
-    lbl.backgroundColor = [UIColor clearColor];
-    lbl.textAlignment = UITextAlignmentCenter;
-    lbl.text = @"Awesome.";
-    [_ticker addSubview:lbl];
+    UILabel *lbl2 = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 100, 150)];
+    lbl2.backgroundColor = [UIColor clearColor];
+    lbl2.textAlignment = UITextAlignmentCenter;
+    lbl2.text = @"Awesome.";
+    lbl2.layer.opacity = 1;
+    [_ticker addSubview:lbl2];
 }
 
 @end
